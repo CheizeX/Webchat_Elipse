c93d64725cde14956243b7988961aab5f4b395ee
Ezequiel Rivas <ezequiel@innovit.cl>
2022-04-20 12:34:56 -0300

    feat: new changes4

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = (data) => {
   return {
     entry: './src/index.tsx',
     output: {
-      path: './dist',
+      path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
     },
     resolve: {
